d385f343748e706634aff91a9ccf8c950874a96e
David Persson <davidpersson@gmx.de>
2016-07-04 17:51:05 +0200

    Update schema.

--- data/schema.sql
+++ data/schema.sql
@@ -1,9 +1,12 @@
 CREATE TABLE `likes` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) unsigned DEFAULT NULL,
+  `session_key` varchar(250) DEFAULT NULL,
   `model` varchar(50) NOT NULL DEFAULT '',
   `foreign_key` int(11) unsigned NOT NULL,
   `count_real` int(11) unsigned NOT NULL DEFAULT '0',
   `count_seed` int(11) unsigned NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `model` (`model`,`foreign_key`)
-) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8;
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
